1c6593a3f6312e2001af8fed1c07da54ac181bc3
Abdul Rahimi <afr114@gmail.com>
2015-12-05 17:51:59 -0800

    add AlertDialogFragment

---
+++ app/src/main/java/com/example/guest/stormy/AlertDialogFragment.java
@@ -0,0 +1,24 @@
+package com.example.guest.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Guest on 12/5/15.
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.button_message, null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
--- app/src/main/java/com/example/guest/stormy/MainActivity.java
+++ app/src/main/java/com/example/guest/stormy/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.guest.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +32,31 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    Log.v(TAG, response.body().string());
+                    if (response.isSuccessful()) {
+                    }
+                    else {
+                        alertUserAboutError();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+
+                }
+        });
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
