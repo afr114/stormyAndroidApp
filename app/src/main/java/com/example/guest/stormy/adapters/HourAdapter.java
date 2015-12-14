3ca950e8b9260956f10ee97f5b1251acf398acdc
Abdul Rahimi <afr114@gmail.com>
2015-12-13 16:09:30 -0800

    refactor listview and recyclerview

--- app/src/main/java/com/example/guest/stormy/adapters/HourAdapter.java
+++ app/src/main/java/com/example/guest/stormy/adapters/HourAdapter.java
@@ -1,11 +1,14 @@
 package com.example.guest.stormy.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.guest.stormy.R;
 import com.example.guest.stormy.weather.Hour;
@@ -19,8 +22,12 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
 
     //setting a constructor to allows us to create it in the activity and set the data
     private Hour[] mHours;
-    public HourAdapter(Hour[] hours){
+    private Context mContext;
+
+    public HourAdapter(Context context, Hour[] hours){
+        mContext = context;
         mHours = hours;
+
     }
 
 
@@ -44,7 +51,7 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
         return mHours.length;
     }
 
-    public class HourViewHolder extends RecyclerView.ViewHolder {
+    public class HourViewHolder extends RecyclerView.ViewHolder implements ViewStub.OnClickListener{
 
         public TextView mTimeLabel;
         public TextView mSummaryLabel;
@@ -58,6 +65,8 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
             mSummaryLabel = (TextView) itemView.findViewById(R.id.summaryLabel);
             mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
             mIconImageView = (ImageView) itemView.findViewById(R.id.iconImageView);
+
+            itemView.setOnClickListener(this);
         }
 
         public void bindHour(Hour hour) {
@@ -65,6 +74,18 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
             mSummaryLabel.setText(hour.getSummary());
             mTemperatureLabel.setText(hour.getTemperature() + "");
             mIconImageView.setImageResource(hour.getIconId());
+        }
+
+        @Override
+        public void onClick(View v) {
+            String time = mTimeLabel.getText().toString();
+            String temperature = mTemperatureLabel.getText().toString();
+            String summary = mSummaryLabel.getText().toString();
+            String message = String.format("At %s it will be %s and %s",
+                    time,
+                    temperature,
+                    summary);
+            Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/com/example/guest/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/example/guest/stormy/ui/DailyForecastActivity.java
@@ -1,5 +1,6 @@
 package com.example.guest.stormy.ui;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Parcelable;
@@ -7,7 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.guest.stormy.R;
 import com.example.guest.stormy.adapters.DayAdapter;
@@ -15,19 +21,43 @@ import com.example.guest.stormy.weather.Day;
 
 import java.util.Arrays;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 //extend the ListActivity when this new activity was first created because it is in a list view
-public class DailyForecastActivity extends ListActivity {
+public class DailyForecastActivity extends Activity {
 
     private Day[] mDays;
+
+    @Bind(android.R.id.list) ListView mListView;
+    @Bind(android.R.id.empty) TextView mEmptyTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
+        ButterKnife.bind(this);
+
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
         mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
         DayAdapter adapter = new DayAdapter(this, mDays);
-        setListAdapter(adapter);
+
+        mListView.setAdapter(adapter);
+        mListView.setEmptyView(mEmptyTextView);
+        
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+                String conditions = mDays[position].getSummary();
+                String highTemp = mDays[position].getTemperatureMax() + "";
+                String message = String.format("Pn %s the high will be %s and it will be %s",
+                        dayOfTheWeek,
+                        highTemp,
+                        conditions);
+                Toast.makeText(DailyForecastActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
-    
 }
